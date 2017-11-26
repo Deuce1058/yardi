434085bfff52d3b154cd4a78cef32ec7db5d65cf
Jim <Deuce1058@users.noreply.github.com>
2017-11-25 20:42:09 -0500

    Delete PasswordPolicySessionBeanRemote.java

--- yardiEJB/ejbModule/com/yardi/ejb/PasswordPolicySessionBeanRemote.java
+++
@@ -1,9 +0,0 @@
-package com.yardi.ejb;
-
-import javax.ejb.Remote;
-import javax.persistence.EntityManager;
-
-@Remote
-public interface PasswordPolicySessionBeanRemote {
-	PpPwdPolicy find(Long rrn);
-}
