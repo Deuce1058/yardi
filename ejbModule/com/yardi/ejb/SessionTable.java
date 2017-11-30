6d0039bebc019cee63e3452c8eee28b1a028f75d
Jim <Deuce1058@users.noreply.github.com>
2017-11-30 02:57:01 -0500

    Add files via upload

---
+++ ejbModule/com/yardi/ejb/SessionTable.java
@@ -0,0 +1,89 @@
+package com.yardi.ejb;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the SESSION_TABLE database table.
+ * 
+ */
+@Entity
+@Table(name="SESSION_TABLE")
+@NamedQuery(name="SessionTable.findAll", query="SELECT s FROM SessionTable s")
+public class SessionTable implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="ST_USER_ID")
+	private String stUserId;
+
+	@Id
+	@Column(name="ST_SESSSION_ID")
+	private String stSesssionId;
+
+	@Column(name="ST_SESSION_TOKEN")
+	private String stSessionToken;
+
+	@Column(name="ST_LAST_REQUEST")
+	private String stLastRequest;
+
+	@Column(name="ST_LAST_ACTIVE")
+	private Timestamp stLastActive;
+
+	@GeneratedValue
+	@Column(name="ST_RRN")
+	private long stRrn;
+
+	public SessionTable() {
+	}
+
+	public String getStSesssionId() {
+		return this.stSesssionId;
+	}
+
+	public void setStSesssionId(String stSesssionId) {
+		this.stSesssionId = stSesssionId;
+	}
+
+	public Timestamp getStLastActive() {
+		return this.stLastActive;
+	}
+
+	public void setStLastActive(Timestamp stLastActive) {
+		this.stLastActive = stLastActive;
+	}
+
+	public String getStLastRequest() {
+		return this.stLastRequest;
+	}
+
+	public void setStLastRequest(String stLastRequest) {
+		this.stLastRequest = stLastRequest;
+	}
+
+	public long getStRrn() {
+		return this.stRrn;
+	}
+
+	public void setStRrn(long stRrn) {
+		this.stRrn = stRrn;
+	}
+
+	public String getStSessionToken() {
+		return this.stSessionToken;
+	}
+
+	public void setStSessionToken(String stSessionToken) {
+		this.stSessionToken = stSessionToken;
+	}
+
+	public String getStUserId() {
+		return this.stUserId;
+	}
+
+	public void setStUserId(String stUserId) {
+		this.stUserId = stUserId;
+	}
+
+}
---
+++ ejbModule/com/yardi/ejb/UserGroup.java
@@ -0,0 +1,54 @@
+package com.yardi.ejb;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the USER_GROUPS database table.
+ * 
+ */
+@Entity
+@Table(name="USER_GROUPS")
+@NamedQuery(name="UserGroup.findAll", query="SELECT u FROM UserGroup u")
+public class UserGroup implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="UG_USER_ID")
+	private String ugUserId;
+
+	@Column(name="UG_GROUP")
+	private int ugGroup;
+
+	@Id
+	@GeneratedValue
+	@Column(name="UG_RRN")
+	private long ugRrn;
+	public UserGroup() {
+	}
+
+	public long getUgRrn() {
+		return this.ugRrn;
+	}
+
+	public void setUgRrn(long ugRrn) {
+		this.ugRrn = ugRrn;
+	}
+
+	public int getUgGroup() {
+		return this.ugGroup;
+	}
+
+	public void setUgGroup(int ugGroup) {
+		this.ugGroup = ugGroup;
+	}
+
+	public String getUgUserId() {
+		return this.ugUserId;
+	}
+
+	public void setUgUserId(String ugUserId) {
+		this.ugUserId = ugUserId;
+	}
+
+}
