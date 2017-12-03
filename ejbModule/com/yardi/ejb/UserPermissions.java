d814029b0c70c8892f8b06056c657901569fe206
Jim <Deuce1058@users.noreply.github.com>
2017-12-02 19:31:42 -0500

    Add files via upload

---
+++ ejbModule/com/yardi/ejb/UserPermissions.java
@@ -0,0 +1,46 @@
+package com.yardi.ejb;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: UserPermissions
+ * Join of UserGroups and GroupPermissions
+ *
+ */
+@Entity
+
+public class UserPermissions implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String ugUserId;
+	private int ugGroup;
+	private int gpPermission;
+
+	public UserPermissions() {
+	}
+   
+	public UserPermissions(String ugUserId, int ugGroup, int gpPermission) {
+		this.ugUserId = ugUserId;
+		this.ugGroup = ugGroup;
+		this.gpPermission = gpPermission;
+	}
+
+	public String getUgUserId() {
+		return ugUserId;
+	}
+	public void setUgUserId(String ugUserId) {
+		this.ugUserId = ugUserId;
+	}
+	public int getUgGroup() {
+		return ugGroup;
+	}
+	public void setUgGroup(int ugGroup) {
+		this.ugGroup = ugGroup;
+	}
+	public int getGpPermission() {
+		return gpPermission;
+	}
+	public void setGpPermission(int gpPermission) {
+		this.gpPermission = gpPermission;
+	}
+}
