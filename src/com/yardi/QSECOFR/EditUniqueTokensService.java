141e0afa375fcfab23958f9fa20127890a77319f
Jim <Deuce1058@users.noreply.github.com>
2020-07-24 16:48:09 -0700

    see https://github.com/Deuce1058/yardi/issues/72
    yardi\
      src\com\yardi\
        QSECOFR\
          EditUniqueTokensService

--- src/com/yardi/QSECOFR/EditUniqueTokensService.java
+++ src/com/yardi/QSECOFR/EditUniqueTokensService.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -40,9 +41,80 @@ public class EditUniqueTokensService extends HttpServlet {
     public EditUniqueTokensService() {
     }
 
+	private void addKeys(String currentPath, JsonNode jsonNode, Map<String, String> map, List<Integer> suffix) {
+		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 0009");
+	    if (jsonNode.isObject()) {
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 000A");
+	        ObjectNode objectNode = (ObjectNode) jsonNode;
+	        Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
+	        String pathPrefix = currentPath.isEmpty() ? "" : currentPath + "-";
+
+	        while (iter.hasNext()) {
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 000D");
+	            Map.Entry<String, JsonNode> entry = iter.next();
+	            addKeys(pathPrefix + entry.getKey(), entry.getValue(), map, suffix);
+	        }
+	    } else if (jsonNode.isArray()) {
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 000F");
+	        ArrayNode arrayNode = (ArrayNode) jsonNode;
+
+	        for (int i = 0; i < arrayNode.size(); i++) {
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 0010"
+						+ "\n "
+						+ "  i="
+						+ i
+						+ "\n"
+						+ "  arrayNode.size()"
+						+ arrayNode.size()
+						);
+	            suffix.add(i + 1);
+	            addKeys(currentPath, arrayNode.get(i), map, suffix);
+
+	            if (i + 1 <arrayNode.size()){
+					System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 0011"
+							+ "\n "
+							+ "  i="
+							+ i
+							+ "\n"
+							+ "  arrayNode.size()="
+							+ arrayNode.size()
+							+ "\n"
+							+ "  suffix.size()="
+							+ suffix.size()
+							);
+	                //suffix.remove(arrayNode.size() - 1);
+	                suffix.remove(suffix.size()-1);
+	            }
+	        } 
+	    } else if (jsonNode.isValueNode()) {
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 000B");
+	        if (currentPath.contains("-")) {
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 000C");
+	            for (int i = 0; i < suffix.size(); i++) {
+					System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 000E");
+	                currentPath += "-" + suffix.get(i);
+	            }
+
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 0012");
+	            suffix = new ArrayList<>();
+	        }
+
+	        ValueNode valueNode = (ValueNode) jsonNode;
+	        map.put(currentPath, valueNode.asText());
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService addKeys() 0013"
+					+ "\n "
+					+ "  currentPath="
+					+ currentPath
+					+ "\n "
+					+ "  valueNode.asText()="
+					+ valueNode.asText()
+					);
+	    }
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//debug
-		System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0000");
+		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0000");
 		//debug
 		String feedback;
 		EditUniqueTokensRequest editRequest;
@@ -58,17 +130,17 @@ public class EditUniqueTokensService extends HttpServlet {
 				ctx = new InitialContext();
 				userProfileBean = (UserProfile)ctx.lookup("java:global/yardiWeb/UserProfileBean");
 				//debug
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0016");
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0016");
 				//debug
 			} catch (NamingException e) {
 				//debug
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0017");
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0017");
 				//debug
 				e.printStackTrace();
 			}
 			session.setAttribute("userProfileBean", userProfileBean);
 			//debug
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 001B");
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 001B");
 			//debug
 		}
 		
@@ -77,17 +149,17 @@ public class EditUniqueTokensService extends HttpServlet {
 				ctx = new InitialContext();
 				uniqueTokenBean = (UniqueTokens)ctx.lookup("java:global/yardiWeb/UniqueTokensBean");
 				//debug
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0018");
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0018");
 				//debug
 			} catch (NamingException e) {
 				e.printStackTrace();
 				//debug
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0019");
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0019");
 				//debug
 			}
 			session.setAttribute("uniqueTokenBean", uniqueTokenBean);
 			//debug
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 001A");
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 001A");
 			//debug
 		}
 		
@@ -100,7 +172,7 @@ public class EditUniqueTokensService extends HttpServlet {
         }
         
 		//debug
-		System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0014"
+		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0014"
 				+ "\n "
 				+ "  formData="
 				+ formData
@@ -111,7 +183,7 @@ public class EditUniqueTokensService extends HttpServlet {
         editRequest = new EditUniqueTokensRequest();
         editRequest = mapper.readValue(formData, EditUniqueTokensRequest.class);
 		//debug
-		System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0001"
+		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0001"
 				+ "\n "
 				+ "  editRequest="
 				+ editRequest
@@ -124,13 +196,13 @@ public class EditUniqueTokensService extends HttpServlet {
 			 * https://stackoverflow.com/questions/48642450/how-to-iterate-all-subnodes-of-a-json-object 
 			 */
 			//debug
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0008");
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0008");
 			//debug
 			updatedTokens = mapper.readValue(editRequest.getUniqueTokensString(),
 					new TypeReference<Vector<EditUniqueTokensRequest>>() {}
 					);
 			//debug
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0015"
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0015"
 					+ "\n "
 					+ "  updatedTokens =["
 					);
@@ -162,7 +234,7 @@ public class EditUniqueTokensService extends HttpServlet {
 		
         if (editRequest.getAction().equals(com.yardi.rentSurvey.YardiConstants.EDIT_USER_PROFILE_REQUEST_ACTION_FIND)) {
     		//debug
-    		System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0002");
+    		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0002");
     		//debug
     		/*
     		 * Need to map this JSON
@@ -190,18 +262,20 @@ public class EditUniqueTokensService extends HttpServlet {
 			editRequest.setMsgDescription(msg[1]);
 			editRequest.setUniqueTokensString(mapper.writeValueAsString(editRequest.getUniqueTokens()));
     		//debug
-    		System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0006"
+    		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0006"
     				+ "\n "
     				+ "  uniqueTokensString="
     				+ editRequest.getUniqueTokensString() 
     				);
     		//debug
-			response.reset();
+    		showResponseHeaders(response);
+			response.resetBuffer();
+    		showResponseHeaders(response);
 			response.setContentType("application/json");
 			PrintWriter out = response.getWriter();
 			formData = mapper.writeValueAsString(editRequest);
     		//debug
-    		System.out.println("com.yardi.QSECOFR EditUniqueTokensService doGet() 0007"
+    		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService doGet() 0007"
     				+ "\n "
     				+ "  formData="
     				+ formData 
@@ -211,19 +285,19 @@ public class EditUniqueTokensService extends HttpServlet {
 			out.flush();
         }
     }
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
+
 	private boolean findTokens(String userName, UserProfile userProfileBean, UniqueTokens uniqueTokenBean, Vector<Unique_Tokens> uniqueTokens, EditUniqueTokensRequest editRequest) {
 		//debug
-		System.out.println("com.yardi.QSECOFR EditUniqueTokensService findTokens() 0003");
+		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService findTokens() 0003");
 		//debug
 
 		if (userProfileBean.find(userName) == null) {
 			//debug
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService findTokens() 0004");
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService findTokens() 0004");
 			//debug
 			editRequest.setUniqueTokens(null);
 			return false;
@@ -232,7 +306,7 @@ public class EditUniqueTokensService extends HttpServlet {
 		uniqueTokens = uniqueTokenBean.findTokens(userName);
 		editRequest.setUniqueTokens(uniqueTokens);
 		//debug
-		System.out.println("com.yardi.QSECOFR EditUniqueTokensService findTokens() 0005"
+		System.out.println("com.yardi.QSECOFR.EditUniqueTokensService findTokens() 0005"
 				+ "\n "
 				+ "  uniqueTokens=" 
 				+ editRequest.getUniqueTokens()
@@ -240,7 +314,35 @@ public class EditUniqueTokensService extends HttpServlet {
 		//debug
 		return true;
 	}
-
+	
+	private void showResponseHeaders(HttpServletResponse response) {
+		//debug 
+		Collection<String> headerNames = response.getHeaderNames();
+		if (headerNames.isEmpty()) {
+			System.out.println("com.yardi.QSECOFR.EditUniqueTokensService showResponseHeaders() 001C headerNames is empty");
+		}
+		for (String n : headerNames) {
+			Collection<String> headerValues = response.getHeaders(n);
+			if (headerValues.isEmpty()) {
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService showResponseHeaders() 001D "
+						+ "\n"
+						+ "   Response header name="
+						+ n
+						+ "   no headerValues");
+			}
+			for (String v :  headerValues) {
+				System.out.println("com.yardi.QSECOFR.EditUniqueTokensService showResponseHeaders() 001E "
+						+ "\n"
+						+ "   Response header name="
+						+ n
+						+ "   Value="
+						+ v
+						);
+			}
+		}
+		//debug
+	}	
+	
 	private void updateTokens(UniqueTokens uniqueTokenBean, Vector<Unique_Tokens> uniqueTokens, Vector<EditUniqueTokensRequest> updatedTokens) {
 		for (EditUniqueTokensRequest r : updatedTokens) {
 			for (int i=0; i < uniqueTokens.size(); i++) {
@@ -284,75 +386,4 @@ public class EditUniqueTokensService extends HttpServlet {
 			}
 		}
 	}
-	
-	private void addKeys(String currentPath, JsonNode jsonNode, Map<String, String> map, List<Integer> suffix) {
-		System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 0009");
-	    if (jsonNode.isObject()) {
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 000A");
-	        ObjectNode objectNode = (ObjectNode) jsonNode;
-	        Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
-	        String pathPrefix = currentPath.isEmpty() ? "" : currentPath + "-";
-
-	        while (iter.hasNext()) {
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 000D");
-	            Map.Entry<String, JsonNode> entry = iter.next();
-	            addKeys(pathPrefix + entry.getKey(), entry.getValue(), map, suffix);
-	        }
-	    } else if (jsonNode.isArray()) {
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 000F");
-	        ArrayNode arrayNode = (ArrayNode) jsonNode;
-
-	        for (int i = 0; i < arrayNode.size(); i++) {
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 0010"
-						+ "\n "
-						+ "  i="
-						+ i
-						+ "\n"
-						+ "  arrayNode.size()"
-						+ arrayNode.size()
-						);
-	            suffix.add(i + 1);
-	            addKeys(currentPath, arrayNode.get(i), map, suffix);
-
-	            if (i + 1 <arrayNode.size()){
-					System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 0011"
-							+ "\n "
-							+ "  i="
-							+ i
-							+ "\n"
-							+ "  arrayNode.size()="
-							+ arrayNode.size()
-							+ "\n"
-							+ "  suffix.size()="
-							+ suffix.size()
-							);
-	                //suffix.remove(arrayNode.size() - 1);
-	                suffix.remove(suffix.size()-1);
-	            }
-	        } 
-	    } else if (jsonNode.isValueNode()) {
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 000B");
-	        if (currentPath.contains("-")) {
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 000C");
-	            for (int i = 0; i < suffix.size(); i++) {
-					System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 000E");
-	                currentPath += "-" + suffix.get(i);
-	            }
-
-				System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 0012");
-	            suffix = new ArrayList<>();
-	        }
-
-	        ValueNode valueNode = (ValueNode) jsonNode;
-	        map.put(currentPath, valueNode.asText());
-			System.out.println("com.yardi.QSECOFR EditUniqueTokensService addKeys() 0013"
-					+ "\n "
-					+ "  currentPath="
-					+ currentPath
-					+ "\n "
-					+ "  valueNode.asText()="
-					+ valueNode.asText()
-					);
-	    }
-	}	
 }
