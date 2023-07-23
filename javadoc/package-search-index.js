08894d2161df8e43a6179a893db76cc6e6cc1e64
Jim <Deuce1058@users.noreply.github.com>
2023-07-23 15:31:20 -0700

    see https://github.com/Deuce1058/yardi/issues/125
    refresh

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.yardi.ejb"},{"l":"com.yardi.ejb.crypto"},{"l":"com.yardi.ejb.model"},{"l":"com.yardi.ejb.test"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.yardi.ejb"},{"l":"com.yardi.ejb.crypto"},{"l":"com.yardi.ejb.model"},{"l":"com.yardi.ejb.test"},{"l":"com.yardi.shared.QSECOFR"},{"l":"com.yardi.shared.rentSurvey"},{"l":"com.yardi.shared.test"},{"l":"com.yardi.shared.userServices"}]
