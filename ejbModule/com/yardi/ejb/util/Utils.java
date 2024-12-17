8822396e3f4bd49cc6607c8916c0e62857bb5441
Jim <Deuce1058@users.noreply.github.com>
2024-12-17 00:03:04 -0800

    https://github.com/Deuce1058/yardi/issues/143
    yardi\
      ejbModule\
        com\yardi\ejb\
          util\
            Utils
            UtilsBean

--- ejbModule/com/yardi/ejb/util/Utils.java
+++ ejbModule/com/yardi/ejb/util/Utils.java
@@ -34,12 +34,6 @@ public interface Utils {
 	<T> boolean isManaged(EntityManager em, T entity);
 
 	/**
-	 * Attempt to roll back the transaction
-	 * @param tx - The transaction to roll back
-	 */
-	void rollback(UserTransaction tx);
-
-	/**
 	 * Log the transaction status
 	 */
 	void txStatus(UserTransaction tx);
