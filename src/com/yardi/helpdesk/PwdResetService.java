c60d94f1508077fef626e26103ddafc0c9df5c68
Jim <Deuce1058@users.noreply.github.com>
2024-12-19 18:29:29 -0800

    see https://github.com/Deuce1058/yardi/issues/145
    yardi\
      src\com\yardi\
        helpdesk\
          PwdResetService

--- src/com/yardi/helpdesk/PwdResetService.java
+++ src/com/yardi/helpdesk/PwdResetService.java
@@ -23,7 +23,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class PwdResetService
+ * Entry point for helpdesk password reset function. Two requests are serviced: find details about the user profile for the heldesk to review before assigning a temporary 
+ * password and assignment of a temporary password that has a life defined by password policy.
  */
 @WebServlet("/doPwdReset")
 public class PwdResetService extends HttpServlet {
@@ -32,6 +33,13 @@ public class PwdResetService extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Determine whether the session contains a reference to {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean PwdResetCtrlBean}.<p> 
+	 * If the session does not have a reference to PwdResetCtrlBean then get the reference from JNDI and save it in the session. Return the reference that came 
+	 * from either the session or JNDI.   
+	 * @param request {@link HttpServletRequest}
+	 * @return a reference to PwdResetCtrlBean
+	 */
 	private PwdResetCtrl checkSession(HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		System.out.println("com.yardi.helpdesk.PwdResetService.checkSession() 0000 "
@@ -61,9 +69,21 @@ public class PwdResetService extends HttpServlet {
 	}
 	
 	/**
-	 * 
+	 * Entry point for helpdesk reset password function.<p>
+	 * <ul>
+	 *   <li>obtain a reference to {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean PwdResetCtrlBean}</li>
+	 *   <li>read the request from the input stream</li>
+	 *   <li>map the raw input stream to the reset password request container {@link com.yardi.shared.helpdesk.ResetPwdRequest ResetPwdRequest}</li>
+	 *   <li>inject the request container into PwdResetCtrlBean</li>
+	 *   <li>if the request is to find a user profile delegate to {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean#findUserDetails() PwdResetCtrlBean.findUserDetails()}<l/i>
+	 *   <li>if the request is password reset delegate to {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean#resetPwd() PwdResetCtrlBean.resetPwd()}</li>
+	 *   <li>feedback from the request is provided in the request container ResetPwdRequest. It is also available from
+	 *   {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean#getFeedback() PwdResetCtrlBean.getFeedback()}</li>
+	 *   <li>respond to the web request</li>
+	 * </ul>
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("com.yardi.helpdesk.PwdResetService.doGet() 000F ");
 	    PwdResetCtrl pwdResetCtrlBean = checkSession(request);
 	    String formdata = readRawBufferedLine(request); 
 	    ResetPwdRequest resetPwdRequest = mapRequestStringToResetPwdRequest(formdata); 
@@ -87,7 +107,12 @@ public class PwdResetService extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
+
+	/**
+	 * Map the request String data to the {@link com.yardi.shared.helpdesk.ResetPwdRequest ResetPwdRequest} container.    
+	 * @param request String read from the input stream 
+	 * @return java representation of the String read from the request stream
+	 */
 	private ResetPwdRequest mapRequestStringToResetPwdRequest(String request) {
 		System.out.println("com.yardi.helpdesk.PwdResetService.mapRequest() 0003 ");
 		ObjectMapper m = new ObjectMapper(); 
@@ -101,6 +126,12 @@ public class PwdResetService extends HttpServlet {
 		
 		return resetPwdRequest;
 	}
+	
+	/**
+	 * Read the raw input stream and store the contents in a String 
+	 * @param request {@link HttpServletRequest}
+	 * @return String containing the data read from the input stream
+	 */
 	private String readRawBufferedLine(HttpServletRequest request) {
 		System.out.println("com.yardi.helpdesk.PwdResetService.readRawBufferedLine() 0005 ");
 		String formData = ""; 		 
@@ -118,7 +149,11 @@ public class PwdResetService extends HttpServlet {
 
 		return formData;
 	}
-	
+
+	/**
+	 * Log the response headers, clear the content of the underlying buffer in the response without clearing headers or status code and log the response headers again.
+	 * @param response {@link HttpServletResponse}
+	 */
 	private void resetBuffer(HttpServletResponse response) {
 		//debug
 		Collection<String> headerNames = response.getHeaderNames();
@@ -173,6 +208,16 @@ public class PwdResetService extends HttpServlet {
 		//debug
 	}
 	
+	/**
+	 * Respond to the web request, {@link com.yardi.helpdesk.PwdResetService#resetBuffer(HttpServletResponse response) clear} the content of the underlying 
+	 * buffer in the response without clearing headers or status code, remove stateful bean {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean#remove() PwdResetCtrlBean}.
+	 * set session attribute "pwdResetCtrlBean" to null and set the reference to PwdResetCtrlBean to null.
+	 * 
+	 * @param request {@link HttpServletRequest}
+	 * @param response {@link HttpServletResponse}
+	 * @param formData String containing the response in JSON format
+	 * @param pwdResetCtrlBean a reference to {@link com.yardi.ejb.helpdesk.PwdResetCtrlBean PwdResetCtrlBean}
+	 */
 	private void webResponse(HttpServletRequest request, HttpServletResponse response, String formData, PwdResetCtrl pwdResetCtrlBean) {
 		System.out.println("com.yardi.helpdesk.PwdResetService.webResponse() 000D ");
 	    try {
@@ -186,7 +231,6 @@ public class PwdResetService extends HttpServlet {
 		    synchronized(session) {
 			    request.getSession().setAttribute("pwdResetCtrlBean", null); 
 		    }
-		    pwdResetCtrlBean = null;
 		} catch (IOException e) {
 			System.out.println("com.yardi.helpdesk.PwdResetService.webResponse() 000E IOException ");
 			e.printStackTrace();
