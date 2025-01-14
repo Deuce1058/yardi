ee0d8e0adf83819da8475ed499c0710ba1b06592
Jim <Deuce1058@users.noreply.github.com>
2025-01-13 17:46:47 -0800

    see https://github.com/Deuce1058/yardi/issues/140#issuecomment-2588567100
    yardi\
      ejbModule\
        com\yardi\ejb\
          UniqueTokensBean

--- ejbModule/com/yardi/ejb/UniqueTokensBean.java
+++ ejbModule/com/yardi/ejb/UniqueTokensBean.java
@@ -128,8 +128,8 @@ public class UniqueTokensBean implements UniqueTokens {
 				"SELECT NEW com.yardi.shared.helpdesk.PwdHistory(u.up1DateAdded, COUNT(u)) " + 
 	    		"FROM Unique_Tokens u " + 
 	    		"WHERE u.up1UserName = :userID " + 
-	    		"ORDER BY t.up1DateAdded DESC, t.up1Rrn DESC" + 
-	    		"GROUP BY u.up1DateAdded",  
+	    		"GROUP BY u.up1DateAdded, u.up1Rrn " +
+	    		"ORDER BY u.up1DateAdded DESC, u.up1Rrn DESC ", 
 	    		PwdHistory.class);
 		qry.setParameter("userID", userID);
 	    return qry.getResultList(); 
