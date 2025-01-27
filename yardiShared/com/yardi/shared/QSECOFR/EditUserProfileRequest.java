7127e82fddecac26a7712ac8fabd9c1b90187e2a
Jim <Deuce1058@users.noreply.github.com>
2025-01-26 20:45:23 -0800

    see https://github.com/Deuce1058/yardi/issues/157
    yardi\
      yardiShared\com\yardi\shared\
        QSECOFR\
          EditUserProfileRequest

--- yardiShared/com/yardi/shared/QSECOFR/EditUserProfileRequest.java
+++ yardiShared/com/yardi/shared/QSECOFR/EditUserProfileRequest.java
@@ -2,7 +2,6 @@ package com.yardi.shared.QSECOFR;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date; 
