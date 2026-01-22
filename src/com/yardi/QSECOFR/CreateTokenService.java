361c1aea3147d3dbf3df3afc6bbe027fa96269a7
Jim <Deuce1058@users.noreply.github.com>
2026-01-22 08:41:38 -0800

    see see https://github.com/Deuce1058/yardi/issues/176
    yardi\
      com\
        yardi\
          QSECOFR\
            CreateTokenService

--- src/com/yardi/QSECOFR/CreateTokenService.java
+++ src/com/yardi/QSECOFR/CreateTokenService.java
@@ -7,14 +7,18 @@ import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yardi.ejb.crypto.Jargon2Bean;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yardi.shared.userServices.PasswordAuthentication;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Convert a user supplied passwords to a token. Entry point for the create token application.
@@ -40,44 +44,16 @@ public class CreateTokenService extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PasswordAuthentication passwordAuthentication = new PasswordAuthentication();
-		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-		String formData = "";
-		
-        if(br != null){
-        	formData = br.readLine();
-        }
-        
-		System.out.println("com.yardi.QSECOFR.CreateTokenService doGet() 0000 formData=" + formData);
-		ObjectMapper mapper = new ObjectMapper();
-		TokenRequest tokenRequest = new TokenRequest();
-		tokenRequest = mapper.readValue(formData, TokenRequest.class);
-		tokenRequest.setPasswordSave(tokenRequest.getPassword());
-		String userToken="";
-		try {
-			userToken = passwordAuthentication.hash(tokenRequest.passwordToChar());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
+		System.out.println("com.yardi.QSECOFR.CreateTokenService.doget() 0000 ");
+		Jargon2Bean jargon2Bean = lookupJargon2Bean(request);
+		String formData = readRawBufferedLine(request);
+		System.out.println("com.yardi.QSECOFR.CreateTokenService.doGet() 0001 formData=" + formData);
+		TokenRequest tokenRequest = mapRequestStringToTokenRequest(formData);
+		processTokenRequest(jargon2Bean, tokenRequest);		
 		tokenRequest.setMsgID("");
 		tokenRequest.setMsgDescription("");
-		tokenRequest.setPassword("");
-
-		if (!(userToken==null) && !(userToken.equals("")) ) {
-			String s [] = com.yardi.shared.rentSurvey.YardiConstants.YRD0000.split("=");
-			tokenRequest.setMsgID(s[0]);
-			tokenRequest.setMsgDescription(s[1]);
-			tokenRequest.setPassword(userToken);
-		}
-		
-		showResponseHeaders(response);
-		response.resetBuffer();
-		showResponseHeaders(response);
-		response.setContentType("application/json");
-		PrintWriter out = response.getWriter();
-		formData = mapper.writeValueAsString(tokenRequest); //convert the feedback to json 
-		out.print(formData);
-		out.flush();
+		ObjectMapper mapper = new ObjectMapper();
+	    webResponse(request, response, mapper.writeValueAsString(tokenRequest)); 
 		return;
 	}
 
@@ -88,36 +64,179 @@ public class CreateTokenService extends HttpServlet {
 		doGet(request, response);
 	}
 
+	private Jargon2Bean lookupJargon2Bean(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		System.out.println("com.yardi.QSECOFR.CreateTokenService.lookupJargon2Bean() 0005 "
+				+ "\n"
+				+ "    session="
+				+ session.getId()
+				);
+		Jargon2Bean jargon2Bean = null;
+
+    	synchronized(session) {
+    		try {
+    			InitialContext ctx = new InitialContext();
+    			jargon2Bean = (Jargon2Bean)ctx.lookup("java:global/yardiWeb/Jargon2Bean");
+    		} catch (NamingException e) {
+    			System.out.println("com.yardi.QSECOFR.CreateTokenService.lookupJargon2Bean() 0006 ");
+    			e.printStackTrace();
+    			return null;
+    		}
+    	}
+    	
+		return jargon2Bean;
+    }
+	
 	/**
-	 * Show headers in the response and their corresponding values for audit purposes. Of particular interest is the session ID. This is used to show that the session ID 
-	 * is being maintained throughout the session.
-	 * @param response HTTP response
+	 * Map the request String data to the {@link com.yardi.QSECOFR.TokenRequest TokenRequest} container.    
+	 * @param request String read from the input stream 
+	 * @return String representation of the data read from the request stream
 	 */
-	private void showResponseHeaders(HttpServletResponse response) {
-		//debug 
+	private TokenRequest mapRequestStringToTokenRequest(String request) {
+		System.out.println("com.yardi.QSECOFR.CreateTokenService.mapRequestStringToTokenRequest() 0009 ");
+		ObjectMapper m = new ObjectMapper(); 
+		TokenRequest tokenRequest = null;
+		try {
+			tokenRequest = m.readValue(request, TokenRequest.class);
+		} catch (Exception e) {
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.mapRequestStringToTokenRequest() 000A Exception ");
+			e.printStackTrace();
+		}
+		
+		return tokenRequest;
+	}
+
+	/**
+	 * Determine what is being requested and call the appropriate com.yeari.ejb.crypto.Jargon2Bean method. 
+	 * @param jargon2Bean reference to com.yeari.ejb.crypto.Jargon2Bean 
+	 * @param tokenRequest reference to com.yardi.QSECOFR.Tokenrequest
+	 */
+	private void processTokenRequest(Jargon2Bean jargon2Bean, TokenRequest tokenRequest) {
+		try {
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.processTokenRequest() 000B ");
+			
+			if (tokenRequest.getAction().equals("hash")) {
+				System.out.println("com.yardi.QSECOFR.CreateTokenService.processTokenRequest() 000C ");
+				tokenRequest.setHash(jargon2Bean.hash(tokenRequest.getPassword()));
+			}
+			
+			if (tokenRequest.getAction().equals("verify")) {
+				System.out.println("com.yardi.QSECOFR.CreateTokenService.processTokenRequest() 000D ");
+				boolean verifed = jargon2Bean.verify(tokenRequest.getPassword(), tokenRequest.getHash());
+				tokenRequest.setVerifiedYn("n");
+				
+				if (verifed) {tokenRequest.setVerifiedYn("y");}
+			}
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.processTokenRequest() 000E Exception ");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Read the raw input stream and store the contents in a String 
+	 * @param request {@link HttpServletRequest}
+	 * @return String containing the data read from the input stream
+	 */
+	private String readRawBufferedLine(HttpServletRequest request) {
+		System.out.println("com.yardi.QSECOFR.CreateTokenService.readRawBufferedLine() 0007");
+		String formData = "";
+		BufferedReader br;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+			
+			if(br != null){ 
+				formData = br.readLine();
+			} 		  
+		} catch (IOException e) {
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.readRawBufferedLine() 0008 Exception ");
+			e.printStackTrace();
+		} 
+
+		return formData;
+	}
+
+	/**
+	 * Log the response headers, clear the content of the underlying buffer in the response without clearing headers or status code and log the response headers again.
+	 * @param response {@link HttpServletResponse}
+	 */
+	private void resetBuffer(HttpServletResponse response) {
 		Collection<String> headerNames = response.getHeaderNames();
 		if (headerNames.isEmpty()) {
-			System.out.println("com.yardi.QSECOFR.CreateTokenService showResponseHeaders() 0001 headerNames is empty");
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.resetBuffer() 0011 headerNames is empty");
 		}
 		for (String n : headerNames) {
 			Collection<String> headerValues = response.getHeaders(n);
 			if (headerValues.isEmpty()) {
-				System.out.println("com.yardi.QSECOFR.CreateTokenService showResponseHeaders() 0002 "
-						+ "\n"
-						+ "   Response header name="
-						+ n
-						+ "   no headerValues");
+				System.out.println("com.yardi.QSECOFR.CreateTokenService.resetBuffer() 0012 "
+					+ "\n"
+					+ "   Response header name="
+					+ n
+					+ "   no headerValues");
 			}
 			for (String v :  headerValues) {
-				System.out.println("com.yardi.QSECOFR.CreateTokenService showResponseHeaders() 0003 "
+				System.out.println("com.yardi.QSECOFR.CreateTokenService.resetBuffer() 0013 "
 						+ "\n"
 						+ "   Response header name="
 						+ n
 						+ "   Value="
 						+ v
-						);
+					);
 			}
 		}
-		//debug
+        response.resetBuffer();
+		headerNames = response.getHeaderNames();
+		if (headerNames.isEmpty()) {
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.resetBuffer() 0014 headerNames is empty");
+		}
+		for (String n : headerNames) {
+			Collection<String> headerValues = response.getHeaders(n);
+			if (headerValues.isEmpty()) {
+				System.out.println("com.yardi.QSECOFR.CreateTokenService.resetBuffer() 0015 "
+					+ "\n"
+					+ "   Response header name="
+					+ n
+					+ "   no headerValues");
+			}
+			for (String v :  headerValues) {
+				System.out.println("com.yardi.QSECOFR.CreateTokenService.resetBuffer() 0016 "
+						+ "\n"
+						+ "   Response header name="
+						+ n
+						+ "   Value="
+						+ v
+					);
+			}
+		}
+	}
+	
+	/**
+	 * Respond to the web request, {@link com.yardi.QSECOFR.CreateTokenService#resetBuffer(HttpServletResponse response) clear} the content of the underlying 
+	 * buffer in the response without clearing headers or status code.
+	 * 
+	 * @param request {@link HttpServletRequest}
+	 * @param response {@link HttpServletResponse}
+	 * @param formData String containing the response in JSON format
+	 */
+	private void webResponse(HttpServletRequest request, HttpServletResponse response, String formData) {
+		System.out.println("com.yardi.QSECOFR.CreateTokenService.webResponse() 000F "
+				+ "\n    "
+				+ formData
+				);
+	    try {
+	    	resetBuffer(response);
+	    	response.setContentType("application/json"); 
+			PrintWriter out = response.getWriter();
+		    out.print(formData); 
+		    out.flush(); 
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.webResponse() 0017 "
+					+ "JSESSIONID="
+					+ request.getSession().getId()
+					);
+		} catch (IOException e) {
+			System.out.println("com.yardi.QSECOFR.CreateTokenService.webResponse() 0010 IOException ");
+			e.printStackTrace();
+		} 
 	}
 }
