b969eedfc7a81084ab792e57209abe035f90a434
Jim <Deuce1058@users.noreply.github.com>
2026-03-27 19:13:58 -0700

    see https://github.com/Deuce1058/yardi/issues/207
    yardi\
      ejbModule\
        com\yardi\ejb\
          model\
            Pwd_Policy

--- ejbModule/com/yardi/ejb/model/Pwd_Policy.java
+++ ejbModule/com/yardi/ejb/model/Pwd_Policy.java
@@ -1,7 +1,17 @@
 package com.yardi.ejb.model;
 
 import java.io.Serializable;
-import jakarta.persistence.*;
+
+import com.yardi.shared.model.PasswordPolicyLike;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 /**
@@ -18,7 +28,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name="PWD_POLICY", schema="DB2ADMIN")
 @NamedQuery(name="Pwd_Policy.findAll", query="SELECT p FROM Pwd_Policy p")
-public class Pwd_Policy implements Serializable {
+public class Pwd_Policy implements Serializable, PasswordPolicyLike {
 	/**
 	 * Column: PP_DAYS<p>
 	 * 
