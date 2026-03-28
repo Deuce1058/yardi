685d0b043b6d2ff586426768ef9c00bc1c48c570
Jim <Deuce1058@users.noreply.github.com>
2026-03-27 17:32:28 -0700

    see https://github.com/Deuce1058/yardi/issues/204#issuecomment-4146127029
    yardi\
      ejbModule\
        com\yardi\ejb\
          EditPasswordPolicy

--- ejbModule/com/yardi/ejb/EditPasswordPolicy.java
+++ ejbModule/com/yardi/ejb/EditPasswordPolicy.java
@@ -1,39 +1,34 @@
 package com.yardi.ejb;
 
-import jakarta.ejb.Local;
-
-import com.yardi.ejb.model.Pwd_Policy;
 import com.yardi.shared.QSECOFR.EditPwdPolicyRequest;
+
+import jakarta.ejb.Local;
 
 /**
  * Template for methods for editing password policy
  */
 @Local
-public interface EditPasswordPolicy  {
+public interface EditPasswordPolicy {
     /**
-     * Return a {@link com.yardi.shared.QSECOFR.EditPwdPolicyRequest com.yardi.shared.QSECOFR.EditPwdPolicyRequest} container.<p>
-     * 
-     * Clients use this method to obtain a container which holds all of the password policy elements that can be modified by the user. 
-     * 
-     * @return a container which holds all of the password policy elements that can be modified by the user.
+	 * A factory that produces an {@link com.yardi.shared.QSECOFR.EditPwdPolicyRequest EditPwdPolicyRequest} using {@link com.yardi.shared.model.PasswordPolicyCopy PasswordPolicyCopy} 
+	 * as the source
+	 * @return {@link com.yardi.shared.QSECOFR.EditPwdPolicyRequest EditPwdPolicyRequest} container which holds all of the password policy elements that can be edited
+	 */
+	EditPwdPolicyRequest fromPolicyCopy();
+    /**
+     * A factory that produces a {@link com.yardi.shared.QSECOFR.EditPwdPolicyRequest EditPwdPolicyRequest} using {@link com.yardi.ejb.model.Pwd_Policy Pwd_Policy} as the source
+     * @return {@link com.yardi.shared.QSECOFR.EditPwdPolicyRequest EditPwdPolicyRequest} container which holds all of the password policy elements that can be modified by the user.
      */
-	EditPwdPolicyRequest getPwd_Policy();
+	EditPwdPolicyRequest fromPwd_Policy();
     /**
-	 * Construct a new {@link com.yardi.ejb.model.Pwd_Policy com.yardi.ejb.model.Pwd_Policy} entity by mapping 
-	 * {@link com.yardi.shared.QSECOFR.EditPwdPolicyRequest com.yardi.shared.QSECOFR.EditPwdPolicyRequest} container to <code>Pwd_Policy</code> entity.
-	 * 
-	 * @param editPwdPolicyRequest a container that holds all the password policy elements which can be modified by the user. 	  
-	 * @return password policy entity
-	 */	
-	Pwd_Policy newPwdPolicy(EditPwdPolicyRequest editPwdPolicyRequest);
-	/**
 	 * Persist a new {@link com.yardi.ejb.model.Pwd_Policy com.yardi.ejb.model.Pwd_Policy} entity.
 	 * 
 	 * @param editPwdPolicyRequest a container which holds all the password policy elements that can be modified
 	 * by the user. 
 	 */ 
 	void persist(EditPwdPolicyRequest editPwdPolicyRequest);
-    /**
+	
+	/**
      * Update all of the password policy elements that can be modified by the user.
 	 * 
 	 * @param editPwdPolicyRequest an <code>EditPwdPolicyRequest</code> container which holds all the password policy elements that can be modified
