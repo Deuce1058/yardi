df0ec4fb7a2b7f48bc6369d2fd0b81d1906d90d0
Jim <Deuce1058@users.noreply.github.com>
2026-04-12 20:47:26 -0700

    see https://github.com/Deuce1058/yardi/issues/205#issuecomment-4233496384
    yardi\
      ejbModule\
        com\yardi\ejb\
          EditPasswordPolicyBean

--- ejbModule/com/yardi/ejb/EditPasswordPolicyBean.java
+++ ejbModule/com/yardi/ejb/EditPasswordPolicyBean.java
@@ -24,7 +24,7 @@ public class EditPasswordPolicyBean implements EditPasswordPolicy {
 	@PersistenceContext(unitName="yardi")
 	private EntityManager em;
 
-	@EJB private PasswordPolicyBean passwordPolicyBean; 
+	@EJB private PasswordPolicy passwordPolicyBean; 
 	
 	/**
 	 * Default constructor 
